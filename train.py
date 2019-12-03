8d98ecc2e9bc4eccae7627838aa94b52b8a06da3
Antoine Prieur <antoine.prieur45@gmail.com>
2019-12-03 14:14:14 -0500

    changed config file

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ wandb.init(project='ml_project', entity='no_name')
 
 LEARNING_RATE = wandb.config.learning_rate
 NB_EPOCHS = wandb.config.n_epochs
+MODEL = wandb.config.model
 DEVICE = torch.device(
     "cuda") if torch.cuda.is_available() else torch.device("cpu")
 
