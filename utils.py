4265da9509a1ed3dce362b8684619555c9f8dbe7
Blanc Cyril <cyrilblancmail@gmail.com>
2019-12-03 13:35:25 -0500

    visualize data

--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
-# Misc.: dataset preprocessing, data visualization, ... 
+# Misc.: dataset preprocessing, data visualization, ...
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def load_data(snr, code, index):
+    pass
+
+
+if __name__ == "__main__":
+    with open('dataset/SNR5/QPSK/wvform_rx_real_rollOff1', 'r') as f:
+        real = [line.strip(' \n') for line in f]
+        real = np.array(real[1::], dtype=np.float64)
+
+    with open('dataset/SNR5/QPSK/wvform_rx_imag_rollOff1', 'r') as f:
+        imag = [line.strip(' \n') for line in f]
+        imag = np.array(imag[1::], dtype=np.float64)
+
+    plt.scatter(real, imag, marker='x')
+    plt.show()
