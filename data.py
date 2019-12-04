uytdfkhfkjyf data leak fix

data.py
@@ -1,15 +1,18 @@
 # Dataset classes
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import torch
 import torchaudio
+from torch.utils.data.sampler import SubsetRandomSampler
+from setup import MODEL
 
 
 class SignalDataset(Dataset):
     def __init__(self, sub_sample=1):
         self.sub_sample = sub_sample
-        self.rolloffs = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.rolloffs = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
         self.n_batch = 500
+        self.fft = torchaudio.transforms.Spectrogram(n_fft=31)
 
     def __getitem__(self, index):
         ro_index = index // self.n_batch
@@ -17,13 +20,18 @@ class SignalDataset(Dataset):
 
         with open(f'dataset/SNR10/QPSK/wvform_rx_real_rollOff{self.rolloffs[ro_index]}_batch{batch}', 'r') as f:
             X = [line.strip(' \n') for line in f]
-            X = np.array(X[1::self.sub_sample], dtype=np.float64)
+        X = np.array(X[1::self.sub_sample], dtype=np.float64)
+        X = torch.from_numpy(X).float()
 
         with open(f'dataset/SNR10/QPSK/sym_tx_rollOff{self.rolloffs[ro_index]}_batch{batch}', 'r') as f:
             y = [line.strip(' \n').split() for line in f]
-            y = np.array(y[1::self.sub_sample], dtype=np.float64)
+        y = np.array(y[1::self.sub_sample], dtype=np.float64)
+        y = torch.from_numpy(y).float()
 
-        return torchaudio.transforms.Spectrogram(n_fft=31)(torch.from_numpy(X).float().unsqueeze(0)), torch.from_numpy(y).float()
+        if MODEL == 'Conv2D':
+            return self.fft(X.unsqueeze(0)), y
+        if MODEL == 'Conv1D':
+            return X, y
 
     def __len__(self):
         return len(self.rolloffs)*self.n_batch
@@ -31,6 +39,7 @@ class SignalDataset(Dataset):
 
 def get_loaders(batch_size, split_percent=0.8):
     dataset = SignalDataset()
+
     train_size = int(split_percent * len(dataset))
     test_size = len(dataset) - train_size
 

train.py
@@ -24,7 +24,7 @@ if os.path.exists(f"weights/{FILE_NAME}.pt"):
 else:
     print("Weights not found")
 
-criterion = torch.nn.L1Loss()
+criterion = torch.nn.L1Loss().to(DEVICE)
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 for i in range(NB_EPOCHS):
@@ -39,9 +39,6 @@ for i in range(NB_EPOCHS):
 
         Y = model(X)
 
-        Y[Y >= 0] = 1.
-        Y[Y < 0] = -1.
-
         loss = criterion(Y, torch.flatten(y, 1))
 
         loss.backward()
@@ -49,6 +46,7 @@ for i in range(NB_EPOCHS):
 
         wandb.log({"loss": loss})
 
+    model.eval()
     print("Evaluation")
     train_accuracy = compute_accuracy(model, train_loader)
     test_accuracy = compute_accuracy(model, valid_loader)